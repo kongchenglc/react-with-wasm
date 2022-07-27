95dda8d5e7260dc66469ed65e1a0b49896495729
cheng.li <cheng.li@shopee.com>
2022-07-27 11:48:26 +0800

    react wasm init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import init, { add, Chart } from "wasm-lib";
+import { main, setup } from "./bootstrap"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ans, setAns] = useState(0);
+  useEffect(() => {
+    init().then(() => {
+      setAns(add(1, 1));
+      setup(Chart);
+      main();
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>1 + 1 = {ans}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
